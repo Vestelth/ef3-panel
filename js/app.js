57502caad23b481d27b3587690f5153c8290e0e3
Vestelth <vestelth@gmail.com>
2017-04-18 00:16:59 +0200

    ready to add chart

---
+++ js/app.js
@@ -0,0 +1,17 @@
+$(function() {
+
+  const mainNavLi = $('.nav-pills li');
+
+  // mainNavLi.on('click', function(event) {
+  //   mainNavLi.each(function(index) {
+  //     if($(this).hasClass('active')){
+  //       $(this).removeClass('active')
+  //     }
+  //   });
+  //
+  //   $(this).addClass('active');
+  // });
+
+
+
+});
